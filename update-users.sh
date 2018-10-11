a766f20fc3b4b3994b677af9978217eabbb55a27
Angel Montes de Oca <joesatriani10@gmail.com>
2018-10-11 17:56:30 -0500

    Update update-users.sh

--- update-users.sh
+++ update-users.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ $# -eq 0 ]; then
-echo "====Es necesaria la contraseña CA como primer argumento===="
+echo "====Es necesaria la IP y la contraseña CA como primer y segundo argumento===="
 else
-ansible-playbook users.yml -e "server=35.188.61.54 ca_password=$1" -t update-users
+ansible-playbook users.yml -e "server=$1 ca_password=$2" -t update-users
 fi
